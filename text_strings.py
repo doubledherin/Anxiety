504277bbd2e3bf7b68fe94f06ab88bfae790f8f7
Wendy Dherin <wendydherin@gmail.com>
2014-03-19 12:22:08 -0700

    Add new file containing text strings

---
+++ text_strings.py
@@ -0,0 +1,192 @@
+game_header = """
+        o 0           ~~~ANXIETY~~~        o 0
+        ^^^                                ^^^\n\n\n
+        \t a game by Wendy Dherin
+        """
+wavy_line = "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+
+bathroom_opener = """
+Your alarm goes off, and you wake up feeling so relaxed and well rested.
+You get out of bed, yawn and stretch, look out the window, and smile.
+It's a gorgeous morning, and you have a great sense of optimism.
+You head into the bathroom to brush your teeth.
+Wow! You're looking great this morning!
+One thing catches your eye though. 
+Your right eye tooth. It just ...
+flashed or something.
+You look closer ... and you see it's discolored.
+Now you feel a slight stirring of uneasiness in the pit of your belly.
+Part of you wants to ignore it.
+But part of you wants to look closer.
+"""
+
+tooth_dialogue = """
+What the?!?
+You see that the eye tooth has a brilliant, bright green eye.
+The eye blinks at you innocently,
+and then you hear a thin, whispery voice.\n
+TOOTH: I am your inner voice. Your true essential self; your deepest wisdom.
+YOU: Shouldn't that be the role of my wisdom teeth?
+TOOTH: You had them extracted, remember?
+We upper eye teeth are heirs to their throne.
+YOU: So why is it just you? What happened to my other eye tooth?
+TOOTH: Oh, him? He's kind of a deadbeat. I'm all you've got now.
+YOU: So why now? What do you want from me?
+TOOTH: The question is, what do you want from me?
+If you can answer all three of my questions correctly,
+I can make you very happy.
+Are you game?
+"""
+
+tooth_brushing = """
+You start brushing your teeth, pretending nothing weird happened just now.
+But something's off.
+Your eye tooth is wiggling as you brush.
+You stop brushing and gingerly take hold of your eye tooth with your fingers.
+As soon as you touch it, it falls into the palm of your hand!
+You look in the mirror and see a ridiculous gap where your tooth once was.
+You close your eyes in anguish. Why you?!
+All of a sudden, like an avalanche, all of your teeth start falling into your mouth.
+You are horrified, and start screaming.
+What do you scream?
+"""
+
+riddle1 = """
+Your eye tooth clears its throat.
+'The first riddle is the easiest,' it says.\n
+'What is the name of a surface with only one side
+and only one boundary component?'
+"""
+
+riddle1_fail = """
+'Wow. I don't think you even tried,' says the eye tooth.
+'I don't know why I even waste my time.' And with that, the eye closes, and
+your tooth goes back to normal.\n
+You decide the whole thing must have been a hallucination.\n
+"""
+
+riddle2 = """
+'That's right!' the tooth says.
+'Hey, remember that '80s band Pop Will Eat Itself?' asks the tooth.
+'They were cool. Anyway, your second riddle is this:'\n
+What is the name of the serpent that eats its own tail?
+"""
+
+riddle2_fail = """
+'Geez louise,' says the eye tooth.
+'That serpent may have its ass up its head,
+but you have it the other way around.'
+And with that, the eye closes, and your tooth goes back to normal.\n
+"""
+
+riddle3 = """
+"'You got it!' says the tooth."
+'Okay, last one, and then I'll grant you a wish.'
+'You're really close!' the eye says, 'Don't screw it up:'\n
+What is the last name
+of the strange loop
+who's most popular work
+is often referred to by its acronym, G.E.B.?\n
+"""
+
+riddle3_fail = """
+You hear a groan.
+'Really?!? That's your answer?!?'
+The eye tooth closes its eye and disappears.
+"""
+
+wishes_opener = """
+'Congratulations!' the eye tooth says, beaming with delight.
+'You've correctly answered all three riddles!!!'\n
+For that, you have your choice of three options, each of them a dream come true:
+1. You can choose to be young again.
+2. You can choose to experience true love.
+3. You can choose to have the ability to fly.\n
+What will it be?
+"""
+
+wishes_corrector = """
+\nThat wasn't one of your choices.
+Do you want:
+1. to be young again
+2. to have true love
+3. the ability to fly?\n
+"""
+
+school_opener = """
+You're suddenly in a large hallway filled with lockers.
+It looks like a high school. In fact ...
+it's YOUR high school!
+You look down and realize you're a teenager again ... \n
+and that you aren't wearing any pants.
+Or underwear, for that matter.\n
+Your heart starts racing.
+The hallway is empty but you look at your watch and realize
+that the bell is going to ring in 2 minutes.
+Then everyone will come pouring out of the classrooms
+to find you buck naked from the waist down.
+You suddenly remember that you have some gym shorts in your locker.
+You can either:\n
+1. Make a run for a hiding place, or
+2. Open your locker and get those shorts!\n
+What's it going to be?
+"""
+
+locker_opener = """
+You run to your locker ...\n
+... and immediately draw a blank.
+For the life of you, you can't remember your combination.
+All you remember is that the first number is in the 30s,
+the second number is in the 20s, and the third number is in the 40s.
+You can squeeze in 10 tries before the bell rings.
+Hurry!
+"""
+
+bomb_opener = """
+CLICK.\n
+Hooray! You did it!
+You open the locker and grab your shorts, quickly putting them on.
+You're about the close it again, when you hear something.\n
+A ticking sound.\n
+You push aside a notebook and freeze.
+There is a ticking time bomb in your locker!!!!
+It is counting down, 15 seconds, 14, 13 ... \n
+Do you:
+1. Try to defuse it and save the school, becoming a hero? Or
+2. Run for your life and save yourself?
+"""
+
+defuse_opener = """
+You see a blue wire and a pink wire.
+You think if you pull out one, you might defuse the bomb.\n
+On the other hand, you have no idea what you're doing.
+"""
+
+defuse_success = """
+\nThe bomb stops ticking!
+The bell rings.
+"""
+
+defuse_fail = """
+\nThe bomb stops ticking!\n
+You're about to breath a sigh of relief,
+When it starts ticking again ... FASTER!!!\n
+5 ... 4 ... 3 ... \n
+You and everyone at the school are doomed.
+You open your mouth to scream your dying words.\n
+What do you scream?
+"""
+
+outro = """
+Phew! It was all just a bad dream.\n
+Now you can go back to sleep and get some much-needed rest...\n
+... if you DARE!!!!!\n\n\n
+~~GAME OVER~~~\n
+"""
+
+quips = [
+    "(Sheesh. Freud would have a field day with that one.)\n", 
+    "(Yikes. That was rough. Maybe you should stay away from dairy before bed.)\n",
+    "(Dang. If that's your dreamworld, I'd hate to see your reality.)\n",
+    "(Ahem. Excuse me, your Freudian slip is showing.)\n"
+    ]
